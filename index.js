4e040a0541bc97b865c30a12d8f1371895bf529b
Sroubek <honza.sroubek@seznam.cz>
2017-12-04 18:51:51 +0100

    I have added two new fillest to project

---
+++ index.js
@@ -0,0 +1,28 @@
+const Calculate = {
+ sum(inputArray) {
+
+   let totalSum = 0;
+   const inputArrayLength = inputArray.length;
+   for (let i = 0; i < inputArrayLength; i++){
+     totalSum += inputArray[i]
+   }
+   return
+ }
+}
+
+module.exports = Calculate;
+
+const Calculate2 = {
+    sum(inputArray) {
+
+      if(inputArray.length === 0){
+        return 0
+      }
+
+      return inputArray.reduce((sum, value) => {
+        return sum + value;
+      })
+    }
+  }
+
+  module.exports = Calculate;
