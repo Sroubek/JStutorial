886b25b76514a6488d8d580efe9782b1381db609
Sroubek <honza.sroubek@seznam.cz>
2017-12-04 14:46:42 +0100

    Initial commit

---
+++ number.js
@@ -0,0 +1,5 @@
+var example = 123456789;
+let roundUp = 1.5;
+Math.round(0.5);
+var rounded = Math.round(roundUp);
+console.log(rounded);
---
+++ string.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+var someString = 'pizza is alright';
+someString = someString.replace('alright', 'wonderful');
+console.log(someString);
